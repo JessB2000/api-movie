f0759e28eb95184f884ef989b5f85dffda965a58
Jessica <jessbarbosa2000@gmail.com>
2022-07-14 02:00:40 -0300

    adição do modules ainda com erros

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { filmesModule } from './filmes/filmes.modules';
+import { generosModule } from './generos/generos.modules';
 
 @Module({
-  imports: [],
+  imports: [
+    filmesModule,
+    generosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
